97e78b2cca3c8ad304de04dbceb4cab297850dc9
VishShindhe <vishwa.shindhe@gmail.com>
2024-05-17 23:25:01 +0530

    Anagram for HR

---
+++ src/com/vish/CodingProblems/AnagramHR.java
@@ -0,0 +1,30 @@
+package CodingProblems;
+
+import java.util.Scanner;
+
+public class AnagramHR {
+    static boolean isAnagram(String a, String b) {
+        if(a.length() != b.length())
+            return false;
+        int indexOfChar;
+        for (int i = 0; i<a.length(); i++) {
+            indexOfChar = b.indexOf(a.charAt(i));
+            if (indexOfChar == -1)
+                return false;
+            // remove first index of str2
+            b = b.substring(0, indexOfChar) + b.substring(indexOfChar + 1);
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
